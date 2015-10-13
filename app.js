5ea29841b0c67d5a58f457ffb72d7d39992429f5
mojoaxel <alex@wunschik.net>
2015-10-13 22:28:08 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var _ = require('lodash-node');
+var fs = require('fs');
+var path = require('path');
+
+var dot = require('graphlib-dot')
+var express = require('express');
+var app = require('express.io')()
+app.http().io()
+
+var graph = dot.read(fs.readFileSync('topology.dot', 'UTF-8'));
+
+/*
+app.io.route('ready', function(req) {
+	req.io.broadcast('new visitor')
+})
+*/
+
+app.use('/static', express.static(path.join(__dirname, '/bower_components')));
+
+// Send client html.
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/client.html')
+});
+
+app.get('/DOTgraph', function(req, res) {
+	res.send(fs.readFileSync('topology.dot', 'UTF-8'));
+});
+
+app.get('/graph', function(req, res) {
+	var nodes = _.map(graph.nodes(), function(node) {
+		console.log(_.extend({ id: node }, graph.node(node)));
+		return _.extend({ id: node }, graph.node(node));
+	});
+	
+	var edges = _.map(graph.edges(), function(edge) {
+		console.log(_.extend(edge, graph.edge(edge)));
+		return _.extend(edge, graph.edge(edge));
+	});
+	
+	res.json({
+		nodes: nodes,
+		edges: edges
+	});
+});
+
+app.get('/api/edges', function(req, res) {
+	res.send(graph._nodes);
+})
+
+app.listen(8080)
